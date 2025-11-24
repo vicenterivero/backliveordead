2dd6d712cc33ba83cae1d4f48a11c029963cdecb
Vicente Rivero <vrivero@dexabyte.com.mx>
2025-11-24 08:55:20 -0500

    permitir cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "*", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
